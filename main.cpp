a751b6ec63f75dea2ac58ef319639c25e7b558da
RevisedJDUDE2 <JDUDEofficial134@gmail.com>
2024-01-25 16:11:03 +0800

    Finaly!!!

--- main.cpp
+++ main.cpp
@@ -6,13 +6,24 @@
 
 LRESULT CALLBACK lpfnWndProcExWin(HWND, UINT, WPARAM, LPARAM);
 
+//				GLOBALS				//
+float red = 0.0f;
+float green = 0.0f;
+float blue = 0.0f;
+
+int colormodr = 1;
+int colormodg = 1;
+int colormodb = 1;
+
 int WINAPI WinMain(HINSTANCE hThis, HINSTANCE hPrev, LPSTR Args, int NCMD) {
+	const int window_height = 800;
+	const int window_width = 600;
 	HWND handle;
 	WNDCLASSEXW windowclassexw = { sizeof(WNDCLASSEXW) };
 	windowclassexw.lpszClassName = L"Dx10";
 	windowclassexw.lpfnWndProc = lpfnWndProcExWin;
 	windowclassexw.hInstance = hThis;
-	windowclassexw.hbrBackground = CreateSolidBrush(RGB(255, 0, 0));
+	windowclassexw.style = CS_VREDRAW | CS_HREDRAW;
 
 	if (!RegisterClassExW(&windowclassexw)) {
 		MessageBoxW(nullptr, L"cannot register window class", L"", MB_OK);
@@ -32,6 +43,55 @@ int WINAPI WinMain(HINSTANCE hThis, HINSTANCE hPrev, LPSTR Args, int NCMD) {
 	IDXGISwapChain* SwapChain;
 	ID3D10RenderTargetView* Renderer;
 
+	DXGI_SWAP_CHAIN_DESC scd;
+	scd.BufferDesc.Height = window_height;
+	scd.BufferDesc.Width = window_width;
+	scd.BufferDesc.RefreshRate.Numerator = 60;
+	scd.BufferDesc.RefreshRate.Denominator = 1;
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	scd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	scd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+
+	scd.SampleDesc.Count = 1;
+	scd.SampleDesc.Quality = 0;
+
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	scd.BufferCount = 1;
+	scd.OutputWindow = handle;
+	scd.Windowed = false;
+	scd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	scd.Flags = 0;
+
+	D3D10CreateDeviceAndSwapChain(0,
+		D3D10_DRIVER_TYPE_HARDWARE, 0, 0, D3D10_SDK_VERSION, &scd,
+		&SwapChain, &Device);
+
+	ID3D10Texture2D* BackBuffer;
+	SwapChain->GetBuffer(0, __uuidof(ID3D10Texture2D),
+		reinterpret_cast<void**>(&BackBuffer));
+
+	Device->CreateRenderTargetView(BackBuffer, 0, &Renderer);
+
+	BackBuffer->Release();
+
+	Device->OMSetRenderTargets(1, &Renderer, NULL);
+
+	D3D10_VIEWPORT vp;
+	vp.TopLeftX = 0;
+	vp.TopLeftY = 0;
+	vp.Height = window_height;
+	vp.Width = window_width;
+	vp.MinDepth = 0.0f;
+	vp.MaxDepth = 1.0f;
+
+	Device->RSSetViewports(1, &vp);
+
+	D3DXCOLOR bgColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	Device->ClearRenderTargetView(Renderer, bgColor);
+
+	SwapChain->Present(0, 0);
+
 	MSG message = {};
 	bool isOpen = true;
 	while (isOpen) {
@@ -43,6 +103,20 @@ int WINAPI WinMain(HINSTANCE hThis, HINSTANCE hPrev, LPSTR Args, int NCMD) {
 			}
 		} else {
 			//RENDER HERE
+			red += colormodr * 0.00005f;
+			green += colormodg * 0.00002f;
+			blue += colormodb * 0.00001f;
+
+			if (red >= 1.0f || red <= 0.0f)
+				colormodr *= -1;
+			if (green >= 1.0f || green <= 0.0f)
+				colormodg *= -1;
+			if (blue >= 1.0f || blue <= 0.0f)
+				colormodb *= -1;
+
+			D3DXCOLOR bgColor(red, green, blue, 1.0f);
+			Device->ClearRenderTargetView(Renderer, bgColor);
+			SwapChain->Present(0, 0);
 		}
 	}
 	UnregisterClassW(windowclassexw.lpszClassName, windowclassexw.hInstance);
